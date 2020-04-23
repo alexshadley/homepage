1d2d16073cd59a4b5f361994018a7cd27cb8663c
Alex Shadley <shadleyalex@gmail.com>
2020-04-23 16:31:44 -0500

    adds cors support

--- summarizer/api.py
+++ summarizer/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 from gensim.summarization import summarize
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
